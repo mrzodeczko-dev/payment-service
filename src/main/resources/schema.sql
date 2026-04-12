ef919e409b4b318b5e0b7f484c1685fdc240b5d2
Michał Rzodeczko <firelight.code@gmail.com>
2026-04-12 13:32:03 +0200

    Delete src/main/resources/schema.sql

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists shedlock(
-    name varchar(64) not null,
-    lock_until timestamp(3) not null,
-    locked_at timestamp(3) not null default current_timestamp(3),
-    locked_by varchar(255) not null,
-    primary key(name)
-);
